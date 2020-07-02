d8202432c3a70511ae9d825e00a3d3ef67851327
Rashi Jaiswal <rashijaiswal2298@gmail.com>
2020-07-02 20:16:36 +0530

    fourth tut done

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
     def create(self, validated_data):
         return Snippet.objects.create(**validated_data)
@@ -17,3 +19,10 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
